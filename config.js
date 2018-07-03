41a3c59bde859f89301eff0d09c4462ea8a72bdf
Navin Ayer <ayernavin@gmail.com>
2018-07-04 00:45:54 +0545

    Initial Commit

---
+++ config.js
@@ -0,0 +1,21 @@
+window.deepConfig = {
+    apiUrl: 'https://api.thedeep.io/api/v1',
+    tableauInit: function(initCallback) {
+        tableau.authType = tableau.authTypeEnum.basic;
+        initCallback();
+    },
+    getAjaxHeader: function() {
+        const { username, password } = tableau;
+        return {
+            'Authorization': 'Basic ' + btoa(username + ":" + password),
+        };
+    },
+    errorHandler: function(error) {
+        if (error.status === 401) {
+            tableau.abortWithError('authentication failed');
+            tableau.abortForAuth();
+        } else {
+            tableau.abortWithError('error pulling data from server');
+        }
+    },
+};
---
+++ projects/projects.js
@@ -0,0 +1,95 @@
+(function() {
+    const {
+        apiUrl,
+        errorHandler,
+        tableauInit,
+        getAjaxHeader,
+    } = window.deepConfig;
+
+    const projectsFields = [
+        'id', 'title', 'description', 'analysis_framework_title',
+        'number_of_leads', 'number_of_users',
+    ];
+    const url = apiUrl + '/projects/?fields=' + projectsFields.join(',');
+
+    // Create the connector object
+    const deepConnector = tableau.makeConnector();
+
+    // Define the schema
+    deepConnector.getSchema = function(schemaCallback) {
+        const cols = [{
+            id: "id",
+            alias: "id",
+            dataType: tableau.dataTypeEnum.int,
+        }, {
+            id: "title",
+            alias: "title",
+            dataType: tableau.dataTypeEnum.string,
+        }, {
+            id: "description",
+            alias: "description",
+            dataType: tableau.dataTypeEnum.string,
+        }, {
+            id: "analysisFrameworkTitle",
+            alias: "analysisFrameworkTitle",
+            dataType: tableau.dataTypeEnum.string,
+        },{
+            id: "numberOfLeads",
+            alias: "numberOfLeads",
+            dataType: tableau.dataTypeEnum.int,
+        }, {
+            id: "numberOfUsers",
+            alias: "numberOfUsers",
+            dataType: tableau.dataTypeEnum.int,
+        }];
+
+        const tableInfo = {
+            id: "projects",
+            alias: "Deep projects Info",
+            columns: cols,	
+        };
+
+        schemaCallback([tableInfo]);
+    };
+
+    // Init function for connector, called during every phase
+    deepConnector.init = tableauInit
+
+    // Download the data
+    deepConnector.getData = function(table, doneCallback) {
+        const getData = function(resp) {
+            const projects = resp.results;
+            const tableData = projects.map(function(project) {
+                return {
+                    id: project.id,
+                    title: project.title,
+                    description: project.description,
+                    analysisFrameworkTitle: project.analysisFrameworkTitle,
+                    numberOfUsers: project.numberOfUsers,
+                    numberOfLeads: project.numberOfLeads,
+                };
+            });
+
+            table.appendRows(tableData);
+            doneCallback();
+        };
+
+        $.ajax({
+            dataType: "json",
+            url: url,
+            headers: getAjaxHeader(),
+            error: errorHandler,
+            success: getData,
+        });
+    };
+
+    tableau.registerConnector(deepConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "Deep Projects"; // This will be the data source name in Tableau
+            tableau.submit(deepConnector); // This sends the connector object to Tableau
+        }).text("Get Deep Projects Data!");;
+    });
+})();
